b3c1973ad844d3ef39662e9308c23913ac134377
Robert Sanins <rob.sanins@lovehoney.co.uk>
2019-05-15 16:06:10 +0100

    2

--- src/Recipe.test.tsx
+++ src/Recipe.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Recipe from './Recipe';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Recipe />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
