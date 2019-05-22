db8ed43712282797e2f6c5e234fd8041d117f60a
Robert Sanins <rob.sanins@lovehoney.co.uk>
2019-05-22 13:19:27 +0100

    replace apikey

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -3,13 +3,12 @@ import "firebase/auth";
 import "firebase/database";
 
 const config = {
-  apiKey: "AIzaSyB1znVn_4CtKjubFMbARXKvpQHlbhEGNaI",
-  appId: "1:994308490877:web:58a33a64a127c5f8",
-  authDomain: "recipe-e09dd.firebaseapp.com",
-  databaseURL: "https://recipe-e09dd.firebaseio.com",
-  messagingSenderId: "994308490877",
-  projectId: "recipe-e09dd",
-  storageBucket: "recipe-e09dd.appspot.com",
+  apiKey: process.env.REACT_APP_DEV_API_KEY,
+  authDomain: process.env.REACT_APP_DEV_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DEV_DATABASE_URL,
+  projectId: process.env.REACT_APP_DEV_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_DEV_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_DEV_MESSAGING_SENDER_ID,
 };
 
 if (!firebase.apps.length) {
